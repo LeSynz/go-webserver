078e74e0548ccb84302d807bf1f94b5d0cec9180
LeSynz <dbrennan@swaay.co.uk>
2025-09-05 12:52:11 +0100

    idk figuring out routes and static file loading

--- main.go
+++ main.go
@@ -1,13 +1,28 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 )
 
 func main() {
+	fmt.Println("Server is starting on http://localhost:8080")
+	
+	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("./static/"))))
+
+	http.HandleFunc("/page", func(w http.ResponseWriter, r *http.Request) {
+		http.ServeFile(w, r, "./static/page.html")
+	})
+
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		if r.URL.Path != "/" {
+			http.NotFound(w, r)
+			return
+		}
 		http.ServeFile(w, r, "./static/index.html")
 	})
+
+
 
 	http.ListenAndServe(":8080", nil)
 }
